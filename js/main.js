3f47c4e424536b5704d5115247997cf008f2887b
azizbekxoshimjonov <azizbekxoshimjonov.veb@gmail.com>
2024-05-09 11:37:06 +0300

    change pasvord

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function moveLeft() {
+    const elBox__litle = document.querySelector('.box__litle');
+    elBox__litle.style.transform = `translateX(32px)`; 
+    // Yoki istalgan o'girishni kiriting
+}
+function moveLeft1(){
+    const elBox__litle = document.querySelector('.box__litle');
+    elBox__litle.style.transform = `translateX(232px)`; 
+}
+function moveLeft2(){
+    const elBox__litle = document.querySelector('.box__litle');
+    elBox__litle.style.transform = `translateX(430px)`; 
+}
+function moveLeft3(){
+    const elBox__litle = document.querySelector('.box__litle');
+    elBox__litle.style.transform = `translateX(605px)`; 
+}
+function moveLeft4(){
+    const elBox__litle = document.querySelector('.box__litle');
+    elBox__litle.style.transform = `translateX(800px)`; 
+}
+function moveLeft5(){
+    const elBox__litle = document.querySelector('.box__litle');
+    elBox__litle.style.transform = `translateX(995px)`; 
+}
